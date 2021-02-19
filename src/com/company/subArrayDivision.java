22c1856a8f93b83d1f01ed6dabd7ade7bfc2a6a8
MrMT-95 <qanteee@gmail.com>
2021-02-19 20:53:34 +0100

    Initial commit

---
+++ src/com/company/subArrayDivision.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class subArrayDivision {
+
+    public void result() {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String[] strings = scanner.nextLine().split("\\s");
+        List<Integer> s = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            s.add(Integer.parseInt(strings[i]));
+        }
+
+
+        int d = scanner.nextInt();
+        int m = scanner.nextInt();
+
+
+        int output = 0;
+        int sum = 0;
+
+        for(int i = 0 ; i <= (s.size() - m) ; i++){
+            for(int j = 0 ; j < m ; j++){
+                sum = sum + s.get(j+i);
+            }
+            if(sum == d){
+                output++;
+            }
+            sum = 0;
+        }
+
+
+
+
+        System.out.println(output);
+    }
+}
---
+++ src/com/company/sumPairs.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class sumPairs {
+
+    public void result() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] numbers = scanner.nextLine().split("\\s");
+        int n = Integer.parseInt(numbers[0]);
+        int k = Integer.parseInt(numbers[1]);
+
+        String[] strings = scanner.nextLine().split("\\s");
+        int[] ar = new int[6];
+
+        for (int i = 0;i<n;i++){
+            ar[i]= Integer.parseInt(strings[i]);
+        }
+
+        int sum = 0;
+        int output = 0;
+
+        for (int i = 0 ; i < n; i++){
+            for (int j = i+1; j<n;j++){
+                sum = ar[i]+ar[j];
+                if (sum%k==0){
+                    output += 1;
+                }
+            }
+        }
+
+        System.out.println(n + "\n" + k + "\n" + Arrays.toString(ar));
+        System.out.println(output);
+
+
+    }
+}
